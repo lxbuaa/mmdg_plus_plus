7ea30dfdb129d2240b53e6e6b94c53d75333f05f
lxbuaa <167331013+lxbuaa@users.noreply.github.com>
2025-04-06 23:32:18 +0800

    Update common_util.py

--- utils/common_util.py
+++ utils/common_util.py
@@ -25,6 +25,38 @@ def forward_model_with_domain(model, x_rgb, x_d, x_ir, modality, domain):
   if modality == 'RGBDIR':
       return model(x_rgb, x_d, x_ir, domain)
 
+def forward_clip_model(model, x_rgb, x_d, x_ir, modality, mode='train', missing=[]):
+    if mode == 'train':
+        if modality == 'RGB':
+            return model(x_rgb)
+        if modality == 'D':
+            return model(x_d)
+        if modality == 'IR':
+            return model(x_ir)
+        if modality == 'RGBD':
+            return model(x_rgb, x_d)
+        if modality == 'DIR':
+            return model(x_d, x_ir)
+        if modality == 'RGBIR':
+            return model(x_rgb, x_ir)
+        if modality == 'RGBDIR':
+            return model(x_rgb, x_d, x_ir, missing)
+    elif mode == 'test':
+        if modality == 'RGB':
+            return model.forward_test(x_rgb)
+        if modality == 'D':
+            return model.forward_test(x_d)
+        if modality == 'IR':
+            return model.forward_test(x_ir)
+        if modality == 'RGBD':
+            return model.forward_test(x_rgb, x_d)
+        if modality == 'DIR':
+            return model.forward_test(x_d, x_ir)
+        if modality == 'RGBIR':
+            return model.forward_test(x_rgb, x_ir)
+        if modality == 'RGBDIR':
+            return model.forward_test(x_rgb, x_d, x_ir, missing)
+
 def mkdir_if_missing(dirname):
   """Create dirname if it is missing."""
   if not os.path.exists(dirname):
